f6a90f9f94bb0f586833eb029c024211312f8650
Tajato <tahjgordon22@gmail.com>
2020-10-10 19:27:10 -0400

    x and o are placed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,41 @@
 window.onload = function () {
     var c = document.getElementById("board");
     var ca = c.getElementsByTagName("div");
-
+    let circleTurns;
+    let insideHTML;
+    const x_class = "X";
+    const circle_class= "O";
+    
     var t;
     for ( t = 0; t < ca.length; t++) {
+       var eachDiv = ca[t];
+        eachDiv.setAttribute("class","square");
+    
 
-        ca[t].setAttribute("class","square");
+    eachDiv.addEventListener("click",whenClicked,{once:true});
+    }
+    function whenClicked(e) {
+        const eachDiv = e.target
+        const activeClass = circleTurns ? x_class : circle_class
+        placeMark(eachDiv,activeClass)
+        swapTurns()
+        console.log("div got clicked!!")
     }
 
-    ca.addEventListener("click",whenClicked,{once:true});
+    function placeMark(eachDiv,activeClass) {
+        
+        eachDiv.classList.add(activeClass)
+        if (eachDiv.className == "square O") {
+            eachDiv.innerHTML = "O"
+        } else {
+            eachDiv.innerHTML = "X"
+        }
+        
+    
+    }
 
-    function whenClicked(e) {
+    function swapTurns() {
+        circleTurns = !circleTurns
         
     }
 
